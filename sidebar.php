c0106c56c41f6a859f1845f71747b7f445cb5994
Osama Alam <mohammad.alam@live.com>
2016-08-07 13:54:39 -0400

    Initial sidebar integration
    Testimonial section

--- sidebar.php
+++ sidebar.php
@@ -13,5 +13,10 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
+	<?php $my_query = new WP_Query( 'category_name = "testimonial"' ); ?>
+	<?php while ($my_query->have_posts()) : $my_query->the_post();
+	get_template_part( 'template-parts/content', get_post_format() );
+	?>
+	<?php endwhile; ?>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
