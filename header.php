f4037c3452df84f2f194a47b6f35bccdb7be1bd0
Osama Alam <mohammad.alam@live.com>
2016-08-08 14:04:56 -0400

    Revamped header - again
    Second time now!

--- header.php
+++ header.php
@@ -23,7 +23,10 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'chictonic' ); ?></a>
-
+<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'chictonic' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav><!-- #site-navigation -->
 	<header id="masthead" class="site-header" role="banner">
 	
 	<?php
@@ -54,9 +57,6 @@
 
 		
 	</header><!-- #masthead -->
-	<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'chictonic' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+	
 
 	<div id="content" class="site-content">
